41175e10cca8c32b9ee3c041121d89c9655fed23
ShianWang <984292420@qq.com>
2021-02-17 21:43:15 +0800

    feat: change source files

---
+++ packages/dynamic-import/async-component.tsx
@@ -0,0 +1,22 @@
+import { useModule } from '@sigi/react'
+import React from 'react'
+
+import { AsyncModule } from './async.module'
+
+export function AsyncComponent() {
+  const [{ list }, dispatcher] = useModule(AsyncModule)
+  const loading = !list ? <div>loading</div> : null
+
+  const title = list instanceof Error ? <h2>{list.message}</h2> : <h2>Async Component</h2>
+
+  const listNodes = Array.isArray(list) ? list.map((value) => <li key={value}>{value}</li>) : null
+  return (
+    <div>
+      {title}
+      <button onClick={dispatcher.fetchList}>fetchList</button>
+      <button onClick={dispatcher.cancel}>cancel</button>
+      {loading}
+      <ul>{listNodes}</ul>
+    </div>
+  )
+}
